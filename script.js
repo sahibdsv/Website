5b5e4d1d492d59e29bdec81d603b58349aa8cf80
sahibdsv <65963341+sahibdsv@users.noreply.github.com>
2026-05-15 00:24:11 -0400

    Set Google Doc embed to Letter ratio (8.5:11) and remove top offset

--- script.js
+++ script.js
@@ -660,7 +660,7 @@ function renderMediaBlock(line) {
 
         return `
             <div class="block-media">
-                <div class="iframe-wrapper loading" style="aspect-ratio: 16 / 9;">
+                <div class="iframe-wrapper loading" style="aspect-ratio: 8.5 / 11;">
                     <iframe src="${escapeHtml(embedUrl)}"
                         class="gdoc-iframe"
                         style="width: 100%; height: 100%; border: none;"
